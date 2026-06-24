ba9edb50e431311979ee73f2181c93c6598591e3
EL MOUSSAOUI Oussama <65275727+oussamaelmoussaoui@users.noreply.github.com>
2026-06-24 19:21:48 +0100

    Update blog title for clarity and focus

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -309,7 +309,7 @@ const about = {
 
 const blog = {
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "Sharing and writing about my pure experiences",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
